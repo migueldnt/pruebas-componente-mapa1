d9ce53280a3757cfae0beff8b17091a9359e4d3d
Miguel <miguel.danto@gmail.com>
2021-04-30 15:40:14 -0500

    actualizacion

--- src/components/CpisMap.vue
+++ src/components/CpisMap.vue
@@ -9,9 +9,10 @@
                 :opacity="0.4"
                 :tooltipContent="row=>row.nomgeo"/>
             <dai-geojson-layer
-                source="capas/cpis2.geojson"
+                source="capas/cpi_30abril21.geojson"
                 :olstyle="cpis_style"
-                :tooltipContent="row=>`<strong>${row.descripcio}</strong><br>${row.municipio}, ${row.nomgeo}`"/>
+                :tooltipContent="row=>`<strong>${row.descripcio}</strong>`"
+                :popupContent="popupCpis"/>
         </dai-basic-map>
         <p class="mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse aut harum deleniti quas, commodi reprehenderit atque necessitatibus est? Ex, earum. Neque fugiat quam deleniti consectetur ipsam quas ex nobis maxime?</p>
     </div>
@@ -22,6 +23,13 @@
 export default {
     data:function(){
         return{
+            popupCpis:(row)=>{
+                //preparar la direccion 
+                let nuevaDireccion = formatoDireccion(row.direccion)
+                let telefono = formatPhoneNumber(row.tel_oficin);
+                let pagina = `<a target="_blank" href="${row.pagina}">${row.pagina}</a>`;
+                return `<strong>${row.descripcio}</strong><br>${nuevaDireccion}<br>Telefono: ${telefono}<br>${pagina}`
+            },
             estados_style:{
                 style:{
                     fill:{color:"white"},
@@ -39,6 +47,24 @@ export default {
             }
         }
     }
+}
+
+function formatoDireccion(direccion){
+    let nuevaDireccion=direccion
+    let numeroDeSegundaComa = nuevaDireccion.split(",").length == 7 ? 5 : 4;
+    let posSalto1 = (nuevaDireccion.split(",",2).join(",").length) + 1;
+    nuevaDireccion = [nuevaDireccion.slice(0,posSalto1),'<br>',nuevaDireccion.slice(posSalto1)].join("");
+    let posSalto2 = (nuevaDireccion.split(",",numeroDeSegundaComa).join(",").length) + 1;
+    nuevaDireccion = [nuevaDireccion.slice(0,posSalto2),'<br>',nuevaDireccion.slice(posSalto2)].join("");
+    return nuevaDireccion
+}
+function formatPhoneNumber(phoneNumberString) {
+  var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+  var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+  }
+  return null;
 }
 </script>
 
